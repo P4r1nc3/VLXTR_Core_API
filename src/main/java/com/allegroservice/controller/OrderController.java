64e72e26036e6149b8d10d88b15f577fdfac4c34
P4r1nc3 <konrad.tupta@gmail.com>
2025-01-07 21:21:40 +0100

    Adjusted OrderController

--- src/main/java/com/allegroservice/controller/OrderController.java
+++ src/main/java/com/allegroservice/controller/OrderController.java
@@ -1,25 +1,56 @@
 package com.allegroservice.controller;
 
-import com.allegroservice.service.OrderService;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
 
 @RestController
 public class OrderController {
 
-    private final OrderService orderService;
+    private final RestTemplate template = new RestTemplate();
+    @Value("${allegro.client-id}")
+    private String clientId;
 
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
+    @Value("${allegro.client-secret}")
+    private String clientSecret;
 
-    @GetMapping("/orders")
-    public String getOrders() {
-        System.out.println("GET /orders");
+    @GetMapping("/token")
+    public String getToken() {
         try {
-            return orderService.fetchOrders();
+            System.out.println("get toekn test");
+            // Kodowanie clientId:clientSecret w Base64
+            String credentials = clientId + ":" + clientSecret;
+            String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
+
+            // Ustawianie nagłówków
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Basic " + encodedCredentials);
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+            // Body żądania
+            String body = "grant_type=client_credentials";
+
+            // Tworzenie żądania
+            HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
+
+            // Wysyłanie żądania na produkcyjne środowisko Allegro
+            ResponseEntity<String> responseEntity = template.exchange(
+                    "https://allegro.pl/auth/oauth/token",
+                    HttpMethod.POST,
+                    requestEntity,
+                    String.class
+            );
+
+            // Odpowiedź z serwera
+            return responseEntity.getBody();
+
         } catch (Exception e) {
-            return "Error fetching orders: " + e.getMessage();
+            return "Error fetching token: " + e.getMessage();
         }
     }
 
