53f3df62992d64ebbb8411a850d1b176030011ff
P4r1nc3 <konrad.tupta@gmail.com>
2025-01-07 20:26:12 +0100

    Initial commit

---
+++ src/main/java/com/allegroservice/AllegroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.allegroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllegroServiceApplication.class, args);
+	}
+
+}
