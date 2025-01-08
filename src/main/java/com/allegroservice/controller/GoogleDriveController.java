cdacd6f6bed0875ae0da4fc0dfc44691633875c2
P4r1nc3 <konrad.tupta@gmail.com>
2025-01-08 10:24:53 +0100

    Remade GET /folder endpoint

--- src/main/java/com/allegroservice/controller/GoogleDriveController.java
+++ src/main/java/com/allegroservice/controller/GoogleDriveController.java
@@ -4,12 +4,12 @@ import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class GoogleDriveController {
@@ -25,7 +25,7 @@ public class GoogleDriveController {
     }
 
     @GetMapping("/folder")
-    public List<String> listFolderContents() {
+    public ResponseEntity<List<File>> listFolderContents() {
         try {
             FileList result = googleDrive.files().list()
                     .setQ("'" + folderId + "' in parents and trashed = false")
@@ -34,13 +34,7 @@ public class GoogleDriveController {
 
             List<File> files = result.getFiles();
 
-            if (files == null || files.isEmpty()) {
-                return List.of("Folder jest pusty.");
-            } else {
-                return files.stream()
-                        .map(file -> "Name: " + file.getName() + ", ID: " + file.getId() + ", Type: " + file.getMimeType())
-                        .collect(Collectors.toList());
-            }
+            return ResponseEntity.ok(files);
         } catch (IOException e) {
             throw new RuntimeException("Błąd podczas pobierania zawartości folderu: " + e.getMessage(), e);
         }
