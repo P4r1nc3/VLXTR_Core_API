6c8e1ee429ae922a61ea1a9a381057f91fcecc7b
P4r1nc3 <konrad.tupta@gmail.com>
2025-01-08 10:14:30 +0100

    Created GoogleDriveController

---
+++ src/main/java/com/allegroservice/controller/GoogleDriveController.java
@@ -0,0 +1,45 @@
+package com.allegroservice.controller;
+
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class GoogleDriveController {
+
+    private final Drive googleDrive;
+
+    private final String folderId = "14RMqM37hTo9o9M1sriGDtVn4RnWzd-ag";
+
+    public GoogleDriveController(Drive googleDrive) {
+        this.googleDrive = googleDrive;
+    }
+
+    @GetMapping("/folder")
+    public List<String> listFolderContents() {
+        try {
+            FileList result = googleDrive.files().list()
+                    .setQ("'" + folderId + "' in parents and trashed = false")
+                    .setFields("files(id, name, mimeType)")
+                    .execute();
+
+            List<File> files = result.getFiles();
+
+            if (files == null || files.isEmpty()) {
+                return List.of("Folder jest pusty.");
+            } else {
+                return files.stream()
+                        .map(file -> "Name: " + file.getName() + ", ID: " + file.getId() + ", Type: " + file.getMimeType())
+                        .collect(Collectors.toList());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Błąd podczas pobierania zawartości folderu: " + e.getMessage(), e);
+        }
+    }
+}
