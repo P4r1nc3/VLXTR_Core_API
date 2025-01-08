de75f71d80c1fc36cfca4b164f2be527789aa45b
P4r1nc3 <konrad.tupta@gmail.com>
2025-01-08 12:00:07 +0100

    Added GET /offers/{offerId}

--- src/main/java/com/allegroservice/controller/AllegroController.java
+++ src/main/java/com/allegroservice/controller/AllegroController.java
@@ -4,6 +4,7 @@ import com.allegroservice.dto.allegro.OffersResponse;
 import com.allegroservice.service.AllegroService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,13 @@ public class AllegroController {
     public ResponseEntity<OffersResponse> getOffers(@RequestHeader("Authorization") String bearerToken) {
         OffersResponse offersResponse = allegroService.fetchOffers(bearerToken);
         return ResponseEntity.ok(offersResponse);
+    }
+
+    @GetMapping("/offers/{offerId}")
+    public ResponseEntity<OffersResponse.Offer> getOffer(
+            @RequestHeader("Authorization") String bearerToken,
+            @PathVariable String offerId) {
+        OffersResponse.Offer offer = allegroService.fetchOffer(bearerToken, offerId);
+        return ResponseEntity.ok(offer);
     }
 }
