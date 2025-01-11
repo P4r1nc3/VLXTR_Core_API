ca155dc7fceef914c37aa9dfa699542d04085cf1
P4r1nc3 <konrad.tupta@gmail.com>
2025-01-11 11:46:41 +0100

    Adjusted ProductRepository

--- src/main/java/com/allegroservice/repository/ProductRepository.java
+++ src/main/java/com/allegroservice/repository/ProductRepository.java
@@ -4,8 +4,10 @@ import com.allegroservice.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
-
+    Optional<Product> findByAllegroOfferId(String allegroOfferId);
 }
