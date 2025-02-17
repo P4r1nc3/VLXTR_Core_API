a5dc293ffab8fd3bcf53ed498cff2255eeaefeca
P4r1nc3 <konrad.tupta@gmail.com>
2025-02-17 20:32:04 +0100

    Allow all origin in cors config

--- src/main/java/com/vlxtrcore/config/CorsConfig.java
+++ src/main/java/com/vlxtrcore/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
