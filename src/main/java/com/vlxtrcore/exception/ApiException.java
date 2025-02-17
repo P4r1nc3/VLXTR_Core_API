82ff06f2d7a37db96913be2ed88264ea05a9581a
P4r1nc3 <konrad.tupta@gmail.com>
2025-02-17 20:30:42 +0100

    Created ApiException

---
+++ src/main/java/com/vlxtrcore/exception/ApiException.java
@@ -0,0 +1,28 @@
+package com.vlxtrcore.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiException extends RuntimeException {
+    private final HttpStatus status;
+    private final String cause;
+    private final String action;
+
+    public ApiException(HttpStatus status, String message, String cause, String action) {
+        super(message);
+        this.status = status;
+        this.cause = cause;
+        this.action = action;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getCauseMessage() {
+        return cause;
+    }
+
+    public String getAction() {
+        return action;
+    }
+}
