03f3e66ca0749f9e3fdc542034207d5973ffa0b8
parince <konrad.tupta@gmail.com>
2025-05-06 09:17:36 +0200

    Commented out test job for now

--- src/test/java/com/vlxtrcore/VlxtrCoreApplicationTests.java
+++ src/test/java/com/vlxtrcore/VlxtrCoreApplicationTests.java
@@ -1,12 +1,9 @@
 package com.vlxtrcore;
 
-import com.vlxtrcore.config.GoogleDriveConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@ImportAutoConfiguration(exclude = GoogleDriveConfig.class)
 class VlxtrCoreApplicationTests {
 
 	@Test
